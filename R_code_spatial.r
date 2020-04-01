9662cafc49611767331033fa9ace641ca6fedbca
EnneFico <62661670+EnneFico@users.noreply.github.com>
2020-04-01 17:53:13 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -21,3 +21,31 @@ bubble(meuse, "zinc")
 
 #Exercise: bubble Copper in red
 bubble(meuse, "Copper", main= "Copper Concentration", col= "red")
+
+## Importing new data from local folder
+# download covid_agg.csv from our teaching site and build a folder called lab in to C:
+# put the covid_agg.csv file into the folder lab
+
+# setting the working directory: lab
+# Windows
+setwd("C:/Users/nicof/OneDrive/Documenti/UNI/UNIBO/esami scelti/2_Ecology/BIO03_MONITORING ECOSYSTEM CHANGES AND FUNCTIONING/Practical/lab")
+
+#inizializate new variable related to the file
+covid <- read.table("covid_agg.csv", head=TRUE)
+
+attach(covid)
+plot(country,cases)
+# make the city names vertical
+plot(country,cases, las=0) # parallel labes
+plot(country,cases, las=1) # orizontal labes
+plot(country,cases, las=2) # ortogonal labes
+plot(country,cases, las=3) # vertical labes
+
+
+plot(country,cases, las=3, cex.axes=0.5)
+
+
+#package ggplot2
+install.packages("ggplot2")
+
+#save the .RData under the menu Files
