c2a6373f4ab6d857128b92f0d2e1d149a00472a5
EnneFico <62661670+EnneFico@users.noreply.github.com>
2020-04-01 16:52:25 +0200

    Create R_code_spatial.r

---
+++ R_code_spatial.r
@@ -0,0 +1,22 @@
+# R code for spatial viow of points
+
+install.packages("sp")
+
+library(sp)
+
+data(meuse)
+
+head(meuse)
+
+# coordinates
+coordinates(meuse) = ~x+y   #alt+126
+
+plot(meuse)
+spplot(meuse, "zinc")
+
+# Exercise: plot the spatial amount of copper
+spplot(meuse, "copper", main= "Copper Concentration")
+
+bubble(meuse, "zinc")
+
+#Exercise: bubble Copper in red
