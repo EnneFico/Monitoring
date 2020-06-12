626fc90a78dfa87dcac59d2c1bfe1f0271f5d4bc
EnneFico <62661670+EnneFico@users.noreply.github.com>
2020-06-12 17:28:12 +0200

    Create R_code_temp_sdm.r

---
+++ R_code_temp_sdm.r
@@ -0,0 +1,57 @@
+#install.packages("sdm")
+library(sdm)
+
+file <- system.file("external/species.shp", package="sdm")
+
+species <- shapefile(file)
+
+plot(species)
+
+plot(species[species$Occurrence == 1,],col='blue',pch=16)
+points(species[species$Occurrence == 0,],col='red',pch=16)
+
+#predictors
+path <- system.file("external", package="sdm")
+lst <- list.files(path=path,pattern='asc$',full.names = T)  
+
+preds <- stack(lst)
+cl <- colorRampPalette(c('blue','orange','red','yellow')) (100)
+plot(preds, col=cl)
+
+
+    plot(preds$elevation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+ 
+
+    plot(preds$tempeerature, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+ 
+
+    
+plot(preds$temperature, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+
+    
+plot(preds$precipitation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+
+
+plot(preds$vegetation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+
+
+d <- sdmData(train=species, predictors=preds)
+
+m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=d, methods = "glm")
+
+p1 <- predict(m1, newdata=preds)
+plot(p1, col=cl)
+#let's put the points
+points(species[species$Occurrence == 1,], pch=16)
+ 
+s1 <- stack(preds, p1)
+plot(s1, col=cl)
+
