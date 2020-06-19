1fba8655052d034b4e32922a2125d7c82db32a9a
EnneFico <62661670+EnneFico@users.noreply.github.com>
2020-06-19 13:44:14 +0200

    Update 14_R_code_no2.r

--- 14_R_code_no2.r
+++ 14_R_code_no2.r
@@ -1,91 +1,10 @@
-# setwd("C:/Users/nicof/lab/EN")
-
-# import all images in one time 
-
-rlistEN <- list.files(pattern = "EN_", full.names = T)
-
-list_rastEN <- lapply(rlistEN, raster)
-
-EN.multitemp <- stack(list_rastEN)
-
-polt(EN.multitemp)
-
-setwd("C:/lab/NO2/")
-
-rlist<-list.files(pattern="EN_")
-
-rlist
-
-import <- lapply(rlist, raster)
-
-NO2.multitemp <-stack(import)
-
-cl <- colorRampPalette(c('darkblue','dark green','yellow'))(100) 
-
-plot(NO2.multitemp,col=cl)
-
-#let's see january and march
-par(mfrow=c(1,2))
-
-plot(NO2.multitemp$EN_0001, col=cl)
-plot(NO2.multitemp$EN_0013, col=cl)
-
-#RGB space
-plotRGB(NO2.multitemp, r=1, g=7, b=13, stretch="lin")
-
-#difference map
-dif <- NO2.multitemp$EN_0013 - NO2.multitemp$EN_0001
-cld <- colorRampPalette(c('blue','white','red'))(100) # 
-plot(dif, col=cld)
-
-#quantitative estimate
-
-boxplot(NO2.multitemp)
-
-boxplot(NO2.multitemp, outline=F)
-
-boxplot(NO2.multitemp, outline=F,horizontal=T)
-
-boxplot(NO2.multitemp, outline=F,horizontal=T,axes=T)
-
-dev.off()
-
-boxplot(NO2.multitemp, outline=F,horizontal=T,axes=T)
-
-plot(NO2.multitemp$EN_0001, NO2.multitemp$EN_0013)
-
-abline(0,1,col="red")
-
-#let's take back the snow data
-
-setwd("C:/snow/")
-
-rlist <- list.files(pattern="snow")
-
- 
-rlist
-
-import <- lapply(rlist, raster)
-
- 
-snow.multitemp <- stack(import)
-
-plot(snow.multitemp$snow2010r, snow.multitemp$snow2020r)
-
-abline(0,1, col="red")
-
-plot(snow.multitemp$snow2000r, snow.multitemp$snow2020r)
-
-abline(0,1,col="red")
-
-
-########################################################
-#########################################################
 
 #R_code_no2.r
+
 #exercise on the reduction of no2 because of the lockdown due to covid19
 #set the working directory within lab/no2
-setwd("/Users/alessandro/lab/no2") #mac
+# setwd("C:/Users/nicof/lab/EN") #PC
+setwd("C:/lab/NO2/")
 #we can import through the lapply() function that applies a certain function to a list of files
 #we need to create a list
 rlist <- list.files(pattern="EN_00")
@@ -139,3 +58,42 @@ boxplot(EN,horizontal=T,axes=T,outline=F)
 plot(EN$EN_0001, EN$EN_0013)
 #we can make the abline (y=x)
 abline(0,1, col="red")
+
+#quantitative estimate
+
+boxplot(NO2.multitemp)
+
+boxplot(NO2.multitemp, outline=F)
+
+boxplot(NO2.multitemp, outline=F,horizontal=T)
+
+boxplot(NO2.multitemp, outline=F,horizontal=T,axes=T)
+
+dev.off()
+
+boxplot(NO2.multitemp, outline=F,horizontal=T,axes=T)
+
+plot(NO2.multitemp$EN_0001, NO2.multitemp$EN_0013)
+
+abline(0,1,col="red")
+
+#lapply with snow database, let's take back the snow data
+setwd("C:/snow/")
+
+rlist <- list.files(pattern="snow")
+
+ 
+rlist
+
+import <- lapply(rlist, raster)
+
+ 
+snow.multitemp <- stack(import)
+
+plot(snow.multitemp$snow2010r, snow.multitemp$snow2020r)
+
+abline(0,1, col="red")
+
+plot(snow.multitemp$snow2000r, snow.multitemp$snow2020r)
+
+abline(0,1,col="red")
