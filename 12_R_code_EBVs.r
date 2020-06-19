c677c3246524651b5c6e1a4ad93ccd6aa4575df3
EnneFico <62661670+EnneFico@users.noreply.github.com>
2020-06-19 13:36:34 +0200

    Update 12_R_code_EBVs.r

--- 12_R_code_EBVs.r
+++ 12_R_code_EBVs.r
@@ -1,56 +1,16 @@
 #R_code_EBVs.r
 
-
-#install.packages("raster")
-library(raster)
-
-sewd("/lab")
-
-snt <- brick("snt_r10.tif")
-
-plot(snt)
-plotRGB(snt,3,2,1, stretch="lin")
-plotRGB(snt,4,3,2, stretch="lin")
-
-
-#for PCA
-install.packages("RStoolbox")
-library(raster)
-library(RStoolbox)
-
-sntpca <- rasterPCA(snt)
-sntpca
-
-summary(sntpca$model)
-#70% of information
-plot(sntpca$model)
-
-plotRGB(sntpca$map, 1, 2, 3, stretch="lin")
-
-#set the moving window
-window <- matrix(1, nrow = 5, ncol = 5)
-window
-
-sd_snt <- focal(sntpca$map$PC1, w= window, fun= sd)
-cl <- colorRampPalette(c('dark blue','green','orange','red'))(100)
-
-plot(sd_snt, col=cl)
-
-par(mfrow=c(1,2))
-plotRGB(snt,4,3,2, stretch="lin", main="original image") 
-plot(sd_snt, col=cl, main="diversity")
-
-
-################################################################
-##################################################################
-
 ### diversity measurement
-setwd("/Users/alessandro/lab") #mac
+setwd("C:/Users/nicof/lab") #PC
+
 install.packages("RStoolbox")
 library(raster)
 library(RStoolbox) # this is for PCA
 #PCA analysis
 sntpca <- rasterPCA(snt)
+
+summary(sntpca$model)
+#70% of information
 
 #to import the image we use the brick() function and we assign it at the vector snt
 snt <- brick("snt_r10.tif")
