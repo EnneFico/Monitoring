98ecc3afc4e60206b4ca0a9c9dc2e10f19af325a
EnneFico <62661670+EnneFico@users.noreply.github.com>
2020-06-19 13:39:18 +0200

    Update 13_R_code_snow.r

--- 13_R_code_snow.r
+++ 13_R_code_snow.r
@@ -1,100 +1,9 @@
-#R_code_snow.r
-
-setwd("~/lab/")
-# setwd("/Users/utente/lab") #mac
-# setwd("C:/lab/") # windows
-
-# install.packages("ncdf4")
-library(ncdf4)
-library(raster)
-
-snowmay <- raster("CopernicusFile.nc")
-cl <- colorRampPalette(c('darkblue','blue','light blue'))(100) 
-
-plot(snowmay,col=cl)
-
-# Slow manner to import the set
-setwd("~/lab/snow/")
-# setwd("/Users/utente/lab/snow/") #mac
-# setwd("C:/lab/snow/") # windows
-
-snow2000 <- raster("snow2000r.tif")
-snow2005 <- raster("snow2005r.tif")
-snow2010 <- raster("snow2010r.tif")
-snow2015 <- raster("snow2015r.tif")
-snow2020 <- raster("snow2020r.tif")
-
-par(mfrow=c(2,3))
-plot(snow2000, col=cl)
-plot(snow2005, col=cl)
-plot(snow2010, col=cl)
-plot(snow2015, col=cl)
-plot(snow2020, col=cl)
-
-################################
-
-#Fast manner to import the set
-rlistsnow <- list.files(pattern="snow20", full.names=T)
-
-##save raster into list
-##con lapply
-
-list_rast <- lapply(rlistsnow, raster)
-snow.multitemp <- stack(list_rast)
-plot(snow.multitemp,col=cl)
-
-# prediction
-# go to IOL and downloand prediction.r into the folder snow
-source("prediction.r")
-
-plot(predicted.snow.2025.norm, col=cl)
-
-#############################################################
-
-# Day 2
-
-setwd("/")
-setwd("C:/Users/nicof/lab/snow")
-
-#Fast manner to import the set
-rlistsnow <- list.files(pattern="snow20", full.names=T)
-
-##save raster into list
-##con lapply
-
-list_rast <- lapply(rlistsnow, raster)
-snow.multitemp <- stack(list_rast)
-plot(snow.multitemp,col=cl)
-
-prediction <- raster("predicted.2025.norm.tif")
-
-writeRaster(prediction, "final.tif")
-
-# save rasters into a stack
-final.stack <- stack(snow.multitemp, prediction)
-
-#plot everything
-plot(final.stack, col=cl)
-
-#make a pdf
-pdf("my_final_exciting_graph.pdf")
-plot(final.stack, col=cl)
-dev.off()
-
-#make a png
-png("my_final_exciting_graph.png")
-plot(final.stack, col=cl)
-dev.off()
-
-
-#######################################################
-#######################################################
 
 # R_code_snow.r
 #1st set the working directory to the folder snow in the folder lab instead of the usual setwd("/Users/alessandro/lab")
 #we create a folder within the folder lab because we will use the function lapply() to search files that comprehend the name within the folder.
 #in this case we don't have other files named snow but it's better to know this
-setwd("/Users/alessandro/lab/snow")
+setwd("C:/Users/nicof/lab/snow")
 
 #we will use raster library and ncdf4(interface with netcdf data, some sort of .tif). Since most of the copernicus are based on nCDF we need to import this library (sometimes is comprehended in raster).
 #once installed we can launch the libraries in R
@@ -203,7 +112,7 @@ predicted.snow.2025.norm <- raster("predicted.snow.2025.norm.tif")
 plot(predicted.snow.2025.norm, col=cl)
 
 ##########################Day 2#######################
-setwd("/Users/alessandro/lab/snow")  #mac
+setwd("C:/Users/nicof/lab/snow")  #PC
 #Exercise: import all of the snow cover images all together
 library(ncdf4)
 library(raster)
