9998b418e646df9565ffd304bb48acd172fea683
EnneFico <62661670+EnneFico@users.noreply.github.com>
2020-06-22 02:14:38 +0200

    Update R_code_exam_project.r

--- R_code_exam_project.r
+++ R_code_exam_project.r
@@ -57,26 +57,106 @@ shp <- shapefile("C:/Users/nicof/MExam/Data/ShapeMajella/Parco Nazionale Majella
 plot(shp)
 
 
+
+setwd("C:/Users/nicof/MExam/Data/)
 #######################################
 #try to stack all band together -input: file.jp2 -output: RasterStack
 
-setwd("C:/Users/nicof/MExam/Data/03012020")
 
-rlist0301 <- list.files(pattern = "T33TVG" )
-import0301 <- lapply(rlist0301,raster)
-m0301.bands <- stack(import0301)
+
+rlist01 <- list.files(pattern = "Jan" )
+import01 <- lapply(rlist01,raster)
+s01.bands <- stack(import01)
 
 #cut off images from a RasterStack with shapefile
-proj4string(m0301.bands)
+proj4string(s01.bands)
 proj4string(M_shp)
 
-mshp <- spTransform(M_shp, proj4string(m0301.bands)) 
+mshp <- spTransform(M_shp, proj4string(s01.bands)) 
 
-m0301_msk <- mask(crop(m0301.bands, extent(mshp)), mshp)  
+m01_msk <- mask(crop(s01.bands, extent(mshp)), mshp)  
 
-plot(m0301_msk)
+plot(m01_msk)
 
+####
 
+rlist02 <- list.files(pattern = "Feb" )
+import02 <- lapply(rlist02,raster)
+s02.bands <- stack(import02)
+
+#cut off images from a RasterStack with shapefile
+proj4string(s02.bands)
+proj4string(M_shp)
+
+mshp <- spTransform(M_shp, proj4string(s02.bands)) 
+
+m02_msk <- mask(crop(s02.bands, extent(mshp)), mshp)
+
+plot(m02_msk)
+
+#####
+
+rlist03 <- list.files(pattern = "Mar" )
+import03 <- lapply(rlist03,raster)
+s03.bands <- stack(import03)
+
+#cut off images from a RasterStack with shapefile
+proj4string(s03.bands)
+proj4string(M_shp)
+
+mshp <- spTransform(M_shp, proj4string(s03.bands)) 
+
+m03_msk <- mask(crop(s03.bands, extent(mshp)), mshp)
+
+plot(m03_msk)
+
+#####
+
+rlist04a <- list.files(pattern = "Apra" )
+import04a <- lapply(rlist04a,raster)
+s04a.bands <- stack(import04a)
+
+#cut off images from a RasterStack with shapefile
+proj4string(s04a.bands)
+proj4string(M_shp)
+
+mshp <- spTransform(M_shp, proj4string(s04a.bands)) 
+
+m04a_msk <- mask(crop(s04a.bands, extent(mshp)), mshp)
+
+plot(m04a_msk)
+
+#####
+rlist04b <- list.files(pattern = "Aprb" )
+import04b <- lapply(rlist04b,raster)
+s04b.bands <- stack(import04b)
+
+#cut off images from a RasterStack with shapefile
+proj4string(s04b.bands)
+proj4string(M_shp)
+
+mshp <- spTransform(M_shp, proj4string(s04b.bands)) 
+
+m04b_msk <- mask(crop(s04b.bands, extent(mshp)), mshp)
+
+plot(m04b_msk)
+
+#####
+rlist05 <- list.files(pattern = "May" )
+import05 <- lapply(rlist05,raster)
+s05.bands <- stack(import05)
+
+#cut off images from a RasterStack with shapefile
+proj4string(s05.bands)
+proj4string(M_shp)
+
+mshp <- spTransform(M_shp, proj4string(05.bands)) 
+
+m05_msk <- mask(crop(s05.bands, extent(mshp)), mshp)
+
+plot(m05_msk)
+
+#########################################à
 #plot single bands
 #B1:blue
 clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
@@ -87,30 +167,87 @@ clr <- colorRampPalette(c('dark red','red','salmon'))(100)
 #B4: with but with NIR (B4) 
 cln <- colorRampPalette(c('red','orange','yellow'))(100) 
 
-#0301 single bands
-plot(m0301_msk$T33TVG_20200103T095401_B02_10m, col = clb)
-#plot(mshp, add=T)
+#if i want border line #plot(mshp, add=T) 
 
-plot(m0301_msk$T33TVG_20200103T095401_B03_10m, col = clg)
-#plot(mshp, add=T)
 
-plot(m0301_msk$T33TVG_20200103T095401_B04_10m, col = clr)
-#plot(mshp, add=T)
+#01 single bands
+par(mfrow=c(2,2))
+plot(m01_msk$Jan_B02_10m, col = clb)
+plot(m01_msk$Jan_B03_10m, col = clg)
+plot(m01_msk$Jan_B04_10m, col = clr)
+plot(m01_msk$Jan_B08_10m, col = cln)
 
-plot(m0301_msk$T33TVG_20200103T095401_B08_10m, col = cln)
-#plot(mshp, add=T)
+#02 single bands
+par(mfrow=c(2,2))
+plot(m02_msk$Feb_B02_10m, col = clb)
+plot(m02_msk$Feb_B03_10m, col = clg)
+plot(m02_msk$Feb_B04_10m, col = clr)
+plot(m02_msk$Feb_B08_10m, col = cln)
 
-#0301 RGB true and false colours
-plotRGB(m0301_msk,3,2,1,scale= "65535", stretch = "lin")
-#plot(mshp, add=T) #border line
+#03 single bands
+par(mfrow=c(2,2))
+plot(m03_msk$Mar_B02_10m, col = clb)
+plot(m03_msk$Mar_B03_10m, col = clg)
+plot(m03_msk$Mar_B04_10m, col = clr)
+plot(m03_msk$Mar_B08_10m, col = cln)
 
-plotRGB(m0301_msk,4,3,2,scale= "65535", stretch = "lin")
-#plot(mshp, add=T) #border line
+#04a single bands
+par(mfrow=c(2,2))
+plot(m04a_msk$Apra_B02_10m, col = clb)
+plot(m04a_msk$Apra_B03_10m, col = clg)
+plot(m04a_msk$Apra_B04_10m, col = clr)
+plot(m04a_msk$Apra_B08_10m, col = cln)
 
+#04b single bands
+par(mfrow=c(2,2))
+plot(m04b_msk$Aprb_B02_10m, col = clb)
+plot(m04b_msk$Aprb_B03_10m, col = clg)
+plot(m04b_msk$Aprb_B04_10m, col = clr)
+plot(m04b_msk$Aprb_B08_10m, col = cln)
+
+#05 single bands
+par(mfrow=c(2,2))
+plot(m05_msk$May_B02_10m, col = clb)
+plot(m05_msk$May_B03_10m, col = clg)
+plot(m05_msk$May_B04_10m, col = clr)
+plot(m05_msk$May_B08_10m, col = cln)
+
+RGB true and false colours
+#RGB
+par(mfrow=c(3,3))
+plotRGB(m01_msk,3,2,1,scale= "65535", stretch = "lin")
+plotRGB(m01_msk,4,3,2,scale= "65535", stretch = "lin")
+#false colour to see evapotraspiration, humidity and clouds
+plotRGB(m01_msk,4,3,2,scale= "65535", stretch = "hist")
+
+plotRGB(m02_msk,3,2,1,scale= "65535", stretch = "lin")
+plotRGB(m02_msk,4,3,2,scale= "65535", stretch = "lin")
+#false colour to see evapotraspiration, humidity and clouds
+plotRGB(m02_msk,4,3,2,scale= "65535", stretch = "hist")
+
+plotRGB(m03_msk,3,2,1,scale= "65535", stretch = "lin")
+plotRGB(m03_msk,4,3,2,scale= "65535", stretch = "lin")
+#false colour to see evapotraspiration, humidity and clouds
+plotRGB(m03_msk,4,3,2,scale= "65535", stretch = "hist"
+
+plotRGB(m04a_msk,3,2,1,scale= "65535", stretch = "lin")
+plotRGB(m04a_msk,4,3,2,scale= "65535", stretch = "lin")
+#false colour to see evapotraspiration, humidity and clouds
+plotRGB(m04a_msk,4,3,2,scale= "65535", stretch = "hist")
+
+plotRGB(m04b_msk,3,2,1,scale= "65535", stretch = "lin")
+plotRGB(m04b_msk,4,3,2,scale= "65535", stretch = "lin")
+#false colour to see evapotraspiration, humidity and clouds
+plotRGB(m04b_msk,4,3,2,scale= "65535", stretch = "hist")
+
+plotRGB(m05_msk,3,2,1,scale= "65535", stretch = "lin")
+plotRGB(m05_msk,4,3,2,scale= "65535", stretch = "lin")
+#false colour to see evapotraspiration, humidity and clouds
+plotRGB(m05_msk,4,3,2,scale= "65535", stretch = "hist")
 ###############################################
 #DVI
-dvi0301 <- m0301_msk$T33TVG_20200103T095401_B02_10m - m0301_msk$T33TVG_20200103T095401_B03_10m
+dvi01 <- m01_msk$T33TVG_20200103T095401_B02_10m - m01_msk$T33TVG_20200103T095401_B03_10m
 
-plot(dvi0301)
+plot(dvi01)
 
 ###############################################
